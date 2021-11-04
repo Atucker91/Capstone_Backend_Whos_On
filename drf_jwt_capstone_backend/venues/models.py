4c07bd02e2088071da867f315258f59a8be4e121
Atucker91 <89651874+Atucker91@users.noreply.github.com>
2021-11-04 12:52:36 -0500

    added venue class table

--- drf_jwt_capstone_backend/venues/models.py
+++ drf_jwt_capstone_backend/venues/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Venue(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    venue_name = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
