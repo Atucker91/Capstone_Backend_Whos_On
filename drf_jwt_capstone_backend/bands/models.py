e2ce1892fee7e50b62ae83ecc4eba2583c2ec49b
Atucker91 <89651874+Atucker91@users.noreply.github.com>
2021-11-04 12:47:49 -0500

    added band class table

--- drf_jwt_capstone_backend/bands/models.py
+++ drf_jwt_capstone_backend/bands/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Band(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    band_name = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=200)
+    song_to_display = models.CharField(max_length=200)
